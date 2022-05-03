68894c0da206a30fbf580fee12554a3c1158a9eb
aulor.xe <aulor.xe@outlook.com>
2022-05-04 02:48:58 +0800

    add type of markdown

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,9 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+// 声明 md 文件
+declare module '*.md' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
