3354aa7580f69c7c20983abd7303777cc3776dcc
MBMll <MBMll@users.noreply.github.com>
2022-05-03 18:50:05 +0000

    deploy: 68894c0da206a30fbf580fee12554a3c1158a9eb

---
+++ assets/InitProject.ec6226b4.js
@@ -0,0 +1,18 @@
+import{o as s,c as t,f as a}from"./index.ffd541d7.js";const e={class:"prose prose-sm m-auto text-left"},o=a(`<pre class="language-bash"><code class="language-bash"><span class="token function">npm</span> create vite@latest
+
+npx eslint --init
+<span class="token comment">#  or </span>
+<span class="token function">npm</span> init @eslint/config
+
+<span class="token function">npm</span> i naive-ui
+<span class="token function">npm</span> i vue-i18n
+<span class="token function">npm</span> i -D @intlify/vite-plugin-vue-i18n
+<span class="token function">npm</span> i -D unplugin-auto-import
+<span class="token function">npm</span> i -D unplugin-vue-components
+<span class="token function">npm</span> i -D vite-plugin-windicss
+<span class="token function">npm</span> i -D vite-plugin-md
+<span class="token function">npm</span> i -D markdown-it-prism
+
+<span class="token function">npm</span> run dev
+
+</code></pre>`,1),p=[o],m={},r={setup(i,{expose:n}){return n({frontmatter:{}}),(c,l)=>(s(),t("div",e,p))}};export{r as default,m as frontmatter};
