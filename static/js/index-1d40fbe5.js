de92268d2e8be0461f090c764f7dd587c597f39f
MBMll <MBMll@users.noreply.github.com>
2022-05-06 17:39:07 +0000

    deploy: d69e3d266b9f9bb6a2bd8ccca7adfd571fc36f61

---
+++ static/js/index-1d40fbe5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-6a0df274.js";const e={};function n(t,_){return" Not Found "}var o=r(e,[["render",n]]);export{o as default};
--- static/js/init-project-c55b1432.js
+++ static/js/init-project-c55b1432.js
@@ -1,4 +1,4 @@
-import{o as s,c as t,f as a}from"./index.ffd541d7.js";const e={class:"prose prose-sm m-auto text-left"},o=a(`<pre class="language-bash"><code class="language-bash"><span class="token function">npm</span> create vite@latest
+import{ah as s,ad as t,ai as a}from"./index-6a0df274.js";const p={class:"prose prose-sm m-auto text-left"},e=a(`<h3>\u521D\u59CB\u5316\u9879\u76EE</h3><pre class="language-bash"><code class="language-bash"><span class="token function">npm</span> create vite@latest
 
 npx eslint --init
 <span class="token comment">#  or </span>
@@ -6,13 +6,17 @@ npx eslint --init
 
 <span class="token function">npm</span> i naive-ui
 <span class="token function">npm</span> i vue-i18n
+<span class="token function">npm</span> i prism-theme-vars
 <span class="token function">npm</span> i -D @intlify/vite-plugin-vue-i18n
 <span class="token function">npm</span> i -D unplugin-auto-import
 <span class="token function">npm</span> i -D unplugin-vue-components
 <span class="token function">npm</span> i -D vite-plugin-windicss
 <span class="token function">npm</span> i -D vite-plugin-md
 <span class="token function">npm</span> i -D markdown-it-prism
+<span class="token function">npm</span> i -D vite-plugin-pages
+<span class="token function">npm</span> i -D eslint-config-prettier
+<span class="token function">npm</span> i -D eslint-plugin-prettier
 
 <span class="token function">npm</span> run dev
 
-</code></pre>`,1),p=[o],m={},r={setup(i,{expose:n}){return n({frontmatter:{}}),(c,l)=>(s(),t("div",e,p))}};export{r as default,m as frontmatter};
+</code></pre>`,2),i=[e],m={},r={setup(o,{expose:n}){return n({frontmatter:{}}),(c,l)=>(t(),s("div",p,i))}};export{r as default,m as frontmatter};
