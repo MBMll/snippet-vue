693172428731d6341ccda7011eff682d36bbc0e8
aulor.xe <aulor.xe@outlook.com>
2023-02-03 01:57:36 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from '@/router'
 import i18n from '@/locale'
@@ -7,8 +9,13 @@ import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
 import 'prism-theme-vars/base.css'
-
+// instance
 const app = createApp(App)
+
 app.use(router)
 app.use(i18n)
+// pinia
+const pinia = createPinia()
+app.use(pinia)
+// mount
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
+import { useUserStore } from '@/store'
 // import routes from 'virtual:generated-pages'
 
 routes.push({
-  path: '/:pathMatch(.*)',
-  name: 'path-match',
-  // redirect: '/404'
-  component: () => import('@/views/404/index.vue')
+    path: '/:pathMatch(.*)',
+    name: 'path-match',
+    // redirect: '/404'
+    component: () => import('@/views/404/index.vue')
 })
 console.log(routes)
-const router = createRouter({ history: createWebHistory(import.meta.env.VITE_BASE_PUBLIC_PATH), routes })
+const router = createRouter({
+    history: createWebHistory(import.meta.env.VITE_BASE_PUBLIC_PATH),
+    routes
+})
+
+router.beforeEach((to) => {
+    const store = useUserStore()
+    if (store.isLoggedIn) {
+        return '/login'
+    }
+})
 
 export default router
